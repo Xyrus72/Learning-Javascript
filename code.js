6ee31c98abb5f87a0bd2d7f5a1b7dcab99961e0f
Xyrus72 <md.refath.hossain@g.bracu.ac.bd>
2023-11-15 00:25:42 +0600

    finished till function

--- code.js
+++ code.js
@@ -1,76 +1,91 @@
-//task-1
-// function processInput(num1) {
-   
-//     var result = num1 ** 3;
-//     return result;
+//                   Variable
+var a= 'st' #var can be changed
+
+let b='str' #let cannot be changed in same blck{} .to use the same variable u gotta have use another block{ }.then u can use the same variable.
+
+const c='me' #this cant change.
+
+
+
+//    If ,else, else if
+
+if (condition){
+    statement;
+}
+else if (another condition){
+    statement;
+}
+else{
+    statement;
+}
+
+
+//        and ,or 
+
+and = &&
+or = ||
+
+
+
+//         LOOPS
+// 1
+for (let i=0'loop starting', i<5 'condition',i++ 'increment'){
+    statement;
+}
+
+// 2
+let obj={
+    a : 90,
+    b : 10,
+    c : 20,
+}
+for (let a in obj) {//#its more like ((for i in a:)
+
+    console.log(obj[a]) //#it will print 90,10,20
+    console.log(a) //#irt will print a,b,c
+}
+
+// 3 while loop
+// while (condition){
+//     statement;
+//     i++
 // }
-// console.log(processInput(4)); 
+let i=0
 
-//task-2
-// function matchFinder(string1, string2) {
-//     if (string1.includes(string2)) {
-//         return true;
-//     } else if (string2.includes(string1)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// var a = prompt();
-// var [b, c] = a.split(',');
-// console.log(matchFinder(b, c));
-
-//task-3
-// function sortMaker(arr){ 
-// if (arr[0]<arr[1]){
-//     [arr[0],arr[1]]=[arr[1],arr[0]];
-//     return arr;
-    
-// }
-// else if (arr[0]==arr[1]){
-//     return 'equal'
-// }
-// else if(arr[0]<0 || arr[1]<0){
-//     return 'Invalid Input'
-// }
-// else{
-//     return arr;
-// }
-// }
-// var arr=[4,4];
-// console.log(sortMaker(arr));
-
-// task-4
-
-// function findAddress(obj) {
-//     var st=''
-
-//     for (var key in obj ){
-//          st+=obj[key]+',';
+while (i<5){
+    console.log(i);
+    i++;
+}
 
 
-//     }
-// st = st.substring(0, st.length - 1);
-// return st
 
-// }
-// var a={street:10,house:'15A',society:'EarthPerfect'}
-// console.log(findAddress(a))
+//      function
+function func_name(para1,para2){
+    statement'
+}
+console.log(func_name(1,2))
 
-//task-5
-// function canPay(changeArray, totalDue) {
-//     var a=changeArray.length;
-//     var c=0
 
-//     for (var i=0; i<a; i++){
-//         c+=changeArray[i];
-//     }
-//     if (c<totalDue){
-//         return false;
-//     }else
-//     {
-//         return true;
-//     }
-// }
-// console.log(canPay([11,2,5],10))
+
+//     type check
+
+let s='sdsdsd'
+console.log(typeof s)
+
+
+//            take user input
+
+//for stirng
+let a=prompt()
+
+//for int
+
+let a=parseInt(prompt())
+
+//for float
+let a=parseFloat(prompt())
+
+
+// V.V note
+//if anyone use those on vs code or any ide then the gotta import
+const prompt = require('prompt-sync')();
